3be88c612c0006b2dde4895f75ca78ea572fcfbb
bloodstrive <you@example.com>
2021-10-16 21:33:38 -0500

    'rest'

---
+++ Extensions.cs
@@ -0,0 +1,16 @@
+using newCat.Dtos;
+using newCat.Models;
+
+namespace newCat
+{
+    public static class Extensions{
+        public static ItemDto AsDto(this Item item){
+            return new ItemDto{
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                CreatedDate = item.CreatedDate
+            };
+        }
+    }
+}
---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,11 @@
+namespace newCat.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host{get;set;}
+        public int Port{get;set;}
+        public string ConnectionString{get{
+            return $"mongodb://{Host}:{Port}";
+        }}
+    }
+}
